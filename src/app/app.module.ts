ee85c54dae6e38a0bd0b048aaf49078b03abbf4f
Ivan Carballeira <icarba@yahoo.es>
2018-05-25 18:57:23 +0200

    Modificacións nos modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,11 @@ import { RouterModule } from '@angular/router';
 // Rutas
 import { APP_ROUTING } from './app.routing';
 
+// Modulos
+import { PagesModule } from './pages/pages.module';
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
 
 
@@ -22,19 +17,12 @@ import { RegisterComponent } from './login/register.component';
   declarations: [
     AppComponent,
     LoginComponent,
-    NopagefoundComponent,
-    DashboardComponent,
-    ProgressComponent,
-    Graficas1Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    PagesComponent,
     RegisterComponent
   ],
   imports: [
     BrowserModule,
     RouterModule,
+    PagesModule,
     APP_ROUTING
   ],
   providers: [],
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,27 +1,14 @@
 import { Routes, RouterModule } from '@angular/router';
 
-import { PagesComponent } from './pages/pages.component';
-
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
 import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
 
 
 const APP_ROUTES: Routes = [
-    { path: '',
-      component: PagesComponent,
-      children: [
-          { path: 'dashboard', component: DashboardComponent },
-          { path: 'progress', component: ProgressComponent },
-          { path: 'graficas1', component: Graficas1Component },
-          { path: '', redirectTo: 'dashboard', pathMatch: 'full' }
-      ]
-    },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
+    { path: '**', redirectTo: '404', pathMatch: 'full' },
     { path: '404', component: NopagefoundComponent },
     { path: '**', redirectTo: '404', pathMatch: 'full' }
 ];
