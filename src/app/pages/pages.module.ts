ed4a3cefa11d7445f44be67121339c00b12172e7
Ivan Carballeira <icarba@yahoo.es>
2018-05-25 18:58:20 +0200

    Agregamos y modificamos módulos

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+
+import { PagesComponent } from './pages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Graficas1Component } from './graficas1/graficas1.component';
+import { SharedModule } from '../shared/shared.module';
+import { PAGES_ROUTING } from './pages.routing';
+
+@NgModule({
+  declarations: [
+    PagesComponent,
+    DashboardComponent,
+    ProgressComponent,
+    Graficas1Component
+  ],
+  exports: [
+    DashboardComponent,
+    ProgressComponent,
+    Graficas1Component
+  ],
+  imports: [
+    SharedModule,
+    PAGES_ROUTING
+  ]
+})
+export class PagesModule {}
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumbsComponent,
+    NopagefoundComponent
+  ],
+  exports: [
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumbsComponent,
+    NopagefoundComponent
+  ],
+  imports: [
+    RouterModule
+  ]
+})
+export class SharedModule {}
